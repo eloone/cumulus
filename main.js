600a1ffabc45d0d0fc2e670d7b8050d47e424b9a
eloone <elodie.rafali@gmail.com>
2014-02-14 01:06:30 +0100

    reactivity on total ok

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ var cumulusApp = angular.module('cumulusApp', []);
 
 cumulusApp.controller('calculateLines', function($scope){
     $scope.numericTotal = 0;
-	$scope.total = $scope.numericTotal;
+	$scope.total = displayTotal();
 	
 	$scope.submitInput = function(){	
 		var matches = /(.*[^\.\d,])((\d+?[\.,]?)?\d+)\D*$/g.exec(this.input);
@@ -33,51 +33,105 @@ cumulusApp.controller('calculateLines', function($scope){
 
 			$scope.numericTotal += amount;
 			
-			$scope.total = $scope.numericTotal.toFixed(2);
+			$scope.total = displayTotal();
 			
 			this.input = '';
 		}
 	
 		return false;
 	}
-	
-	$scope.modifyText = function(e){
-		//e.target.contentEditable = true;
-        console.log(e);
-	};
-    
-    $scope.catchReturn = function(e){
 
-        if(e.keyCode == 39 || e.keyCode == 37 || e.keyCode == 8){
+    function displayTotal(){
+        return $scope.numericTotal.toFixed(2);
+    }
+    
+    $scope.keyStack = [];
+    
+    $scope.filterKeys = function(e){
+
+        //left/right/backspace
+        var allowed = [39, 37, 8],
+            //ctrl/cmd
+            combined = [91, 17];
+        
+        if(allowed.indexOf(e.keyCode) > -1){
             return true;
         }
         
-        if(e.keyCode == 13 || e.keyCode < 48 || e.keyCode > 57){
+        if(combined.indexOf(e.keyCode) > -1){
+            //we have to put keycode on a stack to remember it in ctrl+A case
+            $scope.keyStack.push(e.keyCode);
+            return true;
+        }
+        
+        //enter
+        if(e.keyCode == 13){
             e.preventDefault();
         }
-    }
-    
-    $scope.initChange = function(e){
-         var index = $(e.target).parent('li').attr('id'),
-            original = parseFloat($scope.lines[index].amount);
         
-        $scope.totalToModify = $scope.numericTotal - original;
+        //non digit
+        if(e.keyCode < 48 || e.keyCode > 57){
+            if($scope.keyStack.join(' ').match(/91|17/g)){
+                $scope.keyStack.shift();
+                return true;
+            }else{
+                e.preventDefault();
+            }            
+        } 
+       
+    };
+    
+    $scope.filterReturn = function(e){
+        //enter
+        if(e.keyCode == 13){
+            e.preventDefault();
+        }
+    };
+    
+    $scope.onBlur = function(e){
+         var index = $(e.target).parent('li').attr('id'),
+             value = parseFloat($(e.target).text()),
+             original = $scope.lines[index].amount;
+        
+        if(isNaN(value)){
+            value = 0;
+            //find a way to use this with angular to update total
+            $scope.lines[index].amount = value;
+            //gotta do it by hand
+            $(e.target).text(value);      
+        }
+        
+        $scope.numericTotal += - original + value;
+        
+        $scope.total = displayTotal();
+
     };
     
     $scope.modifyAmount = function(e){
         var index = $(e.target).parent('li').attr('id'),
-            original = parseFloat($scope.lines[index].amount),
+            original = $scope.lines[index].amount,
             value = parseFloat($(e.target).text());
-        
-     
+
+        if(isNaN(value)){
+            value = 0;
+        }
         
         $scope.numericTotal += - original + value;
         
-        $scope.total = $scope.numericTotal.toFixed(2);
+        $scope.total = displayTotal();
         $scope.lines[index].amount = value;
-        console.log($scope.lines[index]);
         
     };
+    
+    //use this to monitor total
+    $scope.$watch(
+  // This is the listener function
+    function() { return $scope.lines; },
+    function(modified, original){
+        console.log(arguments);
+        },
+        true
+    );
 	
 	$scope.lines = [];
 });
